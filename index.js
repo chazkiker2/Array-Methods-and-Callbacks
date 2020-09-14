0c7435071bd452df12dda101ed651bcfe57db106
Chaz Kiker <chazkiker@gmail.com>
2020-09-14 19:43:05 -0400

    finish task 2: getFinals method using .filter()

--- index.js
+++ index.js
@@ -18765,11 +18765,11 @@ console.log(homeTeamName);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+  return data.filter(x => x["Stage"] === "Final");
 };
+console.log(getFinals(fifaData));
+console.log(getFinals(fifaData).length);
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
